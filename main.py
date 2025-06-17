c1d12b9729fb2cff8c9088f89db3e493880f0a4b
PHIL <x1001000@gmail.com>
2025-06-17 17:01:07 +0800

    test

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://claude.ai"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
